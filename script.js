cf37a5b212af7c97d1e6b0f0bc358e89067b6c61
Metr <atwmofficial@gmail.com>
2025-07-10 14:28:45 +0300

    Add a logic to close the sidebar

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-document.getElementById("menuToggle").addEventListener("click", () => {
-  const sidebar = document.getElementById("sidebar");
-  const toggle = document.getElementById("menuToggle");
+const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
+const board = document.getElementById("board");
+const editPanel = document.getElementById("editPanel");
 
-  sidebar.classList.toggle("open");
-  toggle.classList.toggle("hide");
+menuToggle.addEventListener("click", () => {
+  sidebar.classList.add("open");
+  menuToggle.classList.add("hide");
+});
+
+document.addEventListener("click", (event) => {
+  const isClickInsideSidebar = sidebar.contains(event.target);
+  const isClickOnToggle = menuToggle.contains(event.target);
+  const isClickOnBoard = board.contains(event.target);
+  const isClickOnPanel = editPanel.contains(event.target);
+
+  if (!isClickInsideSidebar && !isClickOnToggle && !isClickOnBoard && !isClickOnPanel) {
+    sidebar.classList.remove("open");
+    menuToggle.classList.remove("hide");
+  }
 });
